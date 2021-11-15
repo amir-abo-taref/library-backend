0d49c4c54a238e535010ecbedbd5c24494bccc61
unknown <you@example.com>
2021-11-15 14:31:43 +0200

    some msg

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use('/api/books', require('./routes/books.route'));
 app.use('/api/admin',require('./routes/admin.route'));
 
 
-mongoose.connect('mongodb://localhost:27017/book',{ useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin:am15970@cluster0.y3ixr.mongodb.net/library?retryWrites=true&w=majority',{ useNewUrlParser: true });
 
-app.listen(5001, () => {
+app.listen(process.env.PORT||5001, () => {
     console.log('Server started on port 5001');
 });
